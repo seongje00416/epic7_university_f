0867d8f7fbb9ec7a49be1871074020aafd6b3198
Lim Seongje <96779047+seongje00416@users.noreply.github.com>
2025-01-15 15:19:15 +0900

    ing: 영웅 평가 입력 페이지 제작중

--- src/page/estimating/EstimateHeroPage.tsx
+++ src/page/estimating/EstimateHeroPage.tsx
@@ -1,11 +1,42 @@
-
+import {
+    EstimateContainer,
+    InputContainer,
+    SelectContainer,
+    MainContainer,
+    SelectedContainer,
+    StepContainer,
+    Step, StepText, StepInfo,
+} from "@/style/estimating/Style_EsitmateHeroPage.ts";
 
 const EstimateHeroPage = () => {
 
     return (
-        <div>
+        <MainContainer>
+            <EstimateContainer>
+                <SelectedContainer>
 
-        </div>
+                </SelectedContainer>
+                <SelectContainer>
+                    <StepContainer>
+                        <Step> 
+                            <StepText> 1 </StepText>
+                            <StepInfo> 영웅 선택 </StepInfo>
+                        </Step>
+                        <Step>
+                            <StepText> 2 </StepText>
+                            <StepInfo> 능력치 입력 </StepInfo>
+                        </Step>
+                        <Step>
+                            <StepText> 3 </StepText>
+                            <StepInfo> 아티팩트 및 전용장비 입력 </StepInfo>
+                        </Step>
+                    </StepContainer>
+                    <InputContainer>
+
+                    </InputContainer>
+                </SelectContainer>
+            </EstimateContainer>
+        </MainContainer>
     )
 }
 
---
+++ src/style/estimating/Style_EsitmateHeroPage.ts
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+import {Color} from "@/style/component/CustomColor.ts";
+
+// 메인 컨테이너
+export const MainContainer = styled.div`
+    height: 95vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+`
+
+// 평가 컨텐츠 컨테이너
+export const EstimateContainer = styled.div`
+    display: flex;
+    width: 100%;
+    height: 90%;
+    background-color: whitesmoke;
+    justify-content: center;
+    align-items: center;
+`
+
+// 좌측 선택된 정보 디스플레이 컨테이너
+export const SelectedContainer = styled.div`
+    width: 35%;
+    height: 70%;
+    border-left: 1px solid lightgray;
+    border-top: 1px solid lightgray;
+    border-bottom: 1px solid lightgray;
+`
+
+// 우측 정보 선택 및 입력 디스플레이 컨테이너
+export const SelectContainer = styled.div`
+    width: 55%;
+    height: 70%;
+    border: 1px solid lightgray;
+    display: flex;
+    flex-direction: column;
+`
+
+//      단계 컨테이너
+export const StepContainer = styled.div`
+    display: flex;
+`
+export const Step = styled.div`
+    text-align: center;
+    flex: 1;
+    background-color: ${Color.custom_color_2}
+`
+export const StepText = styled.p`
+    font-size: 18px;
+    border-bottom: 1px solid ${Color.custom_color_1};
+    margin: 0;
+`
+export const StepInfo = styled.p`
+    font-size: 16px;
+    margin: 0;
+`
+
+//      입력 컨테이너
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+export const InputWrapper = styled.div`
+
+`
+export const InputLabel = styled.div`
+
+`
+export const InputBlank = styled.div`
+
+`
+export const Input = styled.input`
+
+`
+
+
